e709c8436a023af307623779091b16702f89e20d
Karo0510 <karma0510@gmail.com>
2022-01-08 15:25:03 +0100

    Fixed minor bugs and added current command test

--- src/test/java/edu/kis/powp/jobs2d/events/SelectCommandVisitorCounterListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectCommandVisitorCounterListener.java
@@ -9,25 +9,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Logger;
 
-public class SelectCommandVisitorCounterListener implements ActionListener {
-    private Logger logger = Logger.getLogger("global");
-    private DriverManager driverManager;
+public class SelectCommandVisitorCounterListener implements ActionListener{
+	private Logger logger = Logger.getLogger("global");
+	private DriverManager driverManager;
 
-    public SelectCommandVisitorCounterListener(DriverManager driverManager) {
-        this.driverManager = driverManager;
-    }
+	public SelectCommandVisitorCounterListener(DriverManager driverManager) {
+		this.driverManager = driverManager;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        logger.info("Testing visitor for driver command.");
-        DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
-
-        if (command == null) {
-            logger.info("No command loaded!");
-        } else {
-            VisitorCounter visitor = new VisitorCounter();
-            command.accept(visitor);
-            logger.info("Counter: " + visitor.getCounter());
-        }
-    }
+	public void actionPerformed(ActionEvent e)
+	{
+		logger.info("Testing visitor for driver command.");
+		DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
+		if (command == null)
+			logger.info("No command loaded");
+		else
+		{
+			VisitorCounter visitor = new VisitorCounter();
+			command.accept(visitor);
+			logger.info("Counter: " + visitor.getCounter());
+		}
+	}
 }
